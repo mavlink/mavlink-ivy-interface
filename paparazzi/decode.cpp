94b1d9edfd4dc6929e3894d3486b5047b723010c
Christophe De Wagter <dewagter@gmail.com>
2011-10-13 09:10:58 +0200

    IVY sample C client

--- paparazzi/decode.cpp
+++ paparazzi/decode.cpp
@@ -1,11 +1,39 @@
+#include <glib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-#include <iostream>
+#include <Ivy/ivy.h>
+#include <Ivy/ivyglibloop.h>
 
 
-int main( int argc, char** argv )
-{
-	std::cout << "Decode" << std::endl;
-
-	return 0;
+static void on_Attitude(IvyClientPtr app, void *user_data, int argc, char *argv[]){
+  guint ac_id = atoi(argv[0]);
+  float estimator_phi = atof(argv[1]);
+  printf("ATTITUDE ac=%d phi=%f\n",ac_id, estimator_phi);
 }
+
+
+int main ( int argc, char** argv) {
+
+  GMainLoop *ml =  g_main_loop_new(NULL, FALSE);
+
+  IvyInit ("Example1", "Example1 READY", NULL, NULL, NULL, NULL);
+  IvyBindMsg(on_Attitude, NULL, "^(\\S*) ATTITUDE (\\S*) (\\S*) (\\S*)");
+
+  IvyStart("127.255.255.255");
+  
+  g_main_loop_run(ml);
+
+  return 0;
+}
+
+//#include <iostream>
+
+
+//int main( int argc, char** argv )
+//{
+//	std::cout << "Decode" << std::endl;
+
+//	return 0;
+//}
 
