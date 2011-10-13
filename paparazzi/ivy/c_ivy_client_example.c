da1e2d201283d4c8cef618dfcd2216950f6b4827
Christophe De Wagter <dewagter@gmail.com>
2011-10-13 09:22:58 +0200

    Plot all IVY traffic

--- paparazzi/ivy/c_ivy_client_example.c
+++ paparazzi/ivy/c_ivy_client_example.c
@@ -5,13 +5,29 @@
 #include <Ivy/ivy.h>
 #include <Ivy/ivyglibloop.h>
 
+#include <string.h>
 
 static void on_Attitude(IvyClientPtr app, void *user_data, int argc, char *argv[]){
-    guint ac_id = atoi(argv[0]);
-    float estimator_phi = atof(argv[1]);
-    float estimator_psi = atof(argv[2]);
-    float estimator_theta = atof(argv[3]);
-    printf("Attitude %d %f %f %f \n",ac_id, estimator_phi, estimator_theta, estimator_psi);
+    int i=0;
+    //guint ac_id = atoi(argv[0]);
+    //float estimator_phi = atof(argv[1]);
+    //float estimator_psi = atof(argv[2]);
+    //float estimator_theta = atof(argv[3]);
+    //printf("Attitude %d %f %f %f \n",ac_id, estimator_phi, estimator_theta, estimator_psi);
+    //printf("Msg %d %s \n",ac_id, argv[1]);
+
+    if (strcmp(argv[0],"ground") == 0)
+    {
+      // printf("ground");
+    }
+    else
+    {
+      for (i=0; i< argc; i++)
+      {
+         printf("%s ",argv[i]);
+      }  
+      printf("\n");
+    }
 }
 
 
@@ -20,7 +36,7 @@ int main ( int argc, char** argv) {
   GMainLoop *ml =  g_main_loop_new(NULL, FALSE);
 
   IvyInit ("Example1", "Example1 READY", NULL, NULL, NULL, NULL);
-  IvyBindMsg(on_Attitude, NULL, "^(\\S*) ATTITUDE (\\S*) (\\S*) (\\S*)");
+  IvyBindMsg(on_Attitude, NULL, "^(\\S*) (.*)");
 
   IvyStart("127.255.255.255");
   
