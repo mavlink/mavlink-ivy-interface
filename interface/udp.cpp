23808b02fa5d05edace31f50640d0f98bb0c5077
MavLab <microuav@gmail.com>
2012-08-08 08:56:19 +0200

    Compatible with APM

--- interface/udp.cpp
+++ interface/udp.cpp
@@ -145,7 +145,12 @@ void udp_send(int ac_id)
 		len = mavlink_msg_to_send_buffer(buf, &msg);
 		bytes_sent = sendto(sock, buf, len, 0, (struct sockaddr*)&gcAddr, sizeof(struct sockaddr_in));
 		
-		
+		/* Send Params */
+		mavlink_msg_param_value_pack(ac_id, 200, &msg, "EXTRA1", 1.23, MAVLINK_TYPE_FLOAT, 1, 0);
+		len = mavlink_msg_to_send_buffer(buf, &msg);
+		bytes_sent = sendto(sock, buf, len, 0, (struct sockaddr*)&gcAddr, sizeof(struct sockaddr_in));
+
+
 		memset(buf, 0, BUFFER_LENGTH);
 		recsize = recvfrom(sock, (void *)buf, BUFFER_LENGTH, 0, (struct sockaddr *)&gcAddr, &fromlen);
 		if (recsize > 0)
